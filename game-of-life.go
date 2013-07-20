f9a8159862b223d64e5f3b48628b43bd83581242
bjartwolf <bjartnes@gmail.com>
2013-07-20 16:18:18 +0200

    init board ok

--- game-of-life.go
+++ game-of-life.go
@@ -1,19 +1,38 @@
 package main
 
 import "fmt"
+
+type World struct {
+    cells [][]Cell
+}
+
 type Cell struct {
     x int
     y int
     alive int
 }
 
-func initWorld(size int) [][]Cell {
-    world := make([][]Cell, size)
-    for i := range world {
-        world[i] = make([]Cell, size)
+func initWorld(size int) World {
+    world := World { make([][]Cell, size)}
+    cells := world.cells
+    for i := range cells {
+        cells[i] = make([]Cell, size)
+        for j := range(cells[i]) {
+            cells[i][j] = Cell{i,j,0}
+        }
     }
     return world
 }
+
+func (w *World) InitBlinker() {
+    world := *w
+    world.cells[4][5].alive = 1
+    world.cells[5][5].alive = 1
+    world.cells[6][5].alive = 1
+    world.cells[5][4].alive = 1
+    world.cells[5][6].alive = 1
+}
+
 func main() {
     fmt.Printf("Hello, world\n")
 }
--- game-of-life_test.go
+++ game-of-life_test.go
@@ -2,12 +2,36 @@ package main
 
 import "testing"
 
-func Test_initialize_world(t *testing.T) {
+// In the future they should not really be in a grid
+// but it is much easier for now...
+func Test_initialize_world_correct_size(t *testing.T) {
     world := initWorld(10)
-    if (len(world) != 10) {
+    if (len(world.cells) != 10) {
         t.Fatal("World should have 10 columns")
     }
-    if (len (world[5]) != 10) {
+    if (len (world.cells[5]) != 10) {
         t.Fatal("World should have 10 rows")
+    }
+}
+
+func Test_initialize_world_correct_positions(t *testing.T) {
+    world := initWorld(10)
+    if (world.cells[5][5].x != 5) {
+        t.Fatal("should be in position 5")
+    }
+}
+
+func Test_initialize_blinker(t *testing.T) {
+    world := initWorld(10)
+    world.InitBlinker()
+    if (world.cells[5][7].alive != 0 ||
+        world.cells[6][7].alive != 0 ||
+        world.cells[4][8].alive != 0) {
+        t.Fatal("All these cells should be dead")
+    }
+    if (world.cells[5][6].alive != 1 ||
+        world.cells[6][5].alive != 1 ||
+        world.cells[4][5].alive != 1) {
+        t.Fatal("All these cells should be alive")
     }
 }
